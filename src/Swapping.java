714220be817f65e36585d90e89dc068f15eabcbf
abhishek6252 <abhishek.bhardwaj_cs20@gla.ac.in>
2022-06-12 11:01:41 +0530

    Swapping of 2 no. using XOR(^) operation.

---
+++ src/Swapping.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Swapping {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of two no. a and b simultaneously:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println("Value of a after swapping:"+a);
+        System.out.println("Value of b after swapping:"+b);
+    }
+}
