4a7c567d65120eaf79bdb67f4271a1587c4529cc
abhishek6252 <abhishek.bhardwaj_cs20@gla.ac.in>
2022-06-20 22:36:06 +0530

    Digits of a number.

---
+++ src/Loops/DigitsOfNum.java
@@ -0,0 +1,14 @@
+package Loops;
+import java.util.Scanner;
+public class DigitsOfNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rem;
+        while(n>0){
+            rem = n%10;
+            n/=10;
+            System.out.println(rem);
+        }
+    }
+}
