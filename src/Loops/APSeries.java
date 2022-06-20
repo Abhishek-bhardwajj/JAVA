c5d8c88cf3cce1fa4ec4bef8759a4ad3d2e65154
abhishek6252 <abhishek.bhardwaj_cs20@gla.ac.in>
2022-06-20 23:19:16 +0530

    Arithmetic Progression Series.

---
+++ src/Loops/APSeries.java
@@ -0,0 +1,19 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class APSeries {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a and d");
+        int a = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.println("Enter the number of elements");
+        int n = sc.nextInt();
+        int term = a;
+        for(int i = 0; i<n; i++){
+            System.out.println(term+" ");
+            term+=d;
+        }
+    }
+}
